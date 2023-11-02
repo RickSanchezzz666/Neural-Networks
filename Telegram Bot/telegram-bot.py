0879d9fd79ce1d3e57e90276333d4e63345ecb32
RickSanchezzz666 <lapiyktube@gmail.com>
2023-11-02 14:48:38 +0200

    bot

---
+++ Telegram Bot/telegram-bot.py
@@ -0,0 +1,21 @@
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+
+def handle_message(update, context):
+    message = update.message
+    chat_id = message.chat_id
+    text = message.text
+
+    response = 'Thanks for your message: ' + text
+    context.bot.send_message(chat_id=chat_id, text=response)
+
+
+bot_token = '6668740803:AAGLqtilUw1sv4MwftOxc7V9dfoqtFFXbVg'
+bot = telegram.Bot(token=bot_token)
+updater = Updater(token=bot_token, use_context=True)
+dispatcher = updater.dispatcher
+
+message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
+dispatcher.add_handler(message_handler)
+
+updater.start_polling()
